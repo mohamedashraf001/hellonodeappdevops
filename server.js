2eecc5d2e24008faa1373bc5c4f07c859fef77d1
mohamedashraf001 <mohamedashraf11ma11@gmial.com>
2025-09-24 02:25:07 +0300

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js 🚀');
+  res.send('Hello mohamed from Node.js 🚀');
 });
 
 app.listen(PORT, () => {
